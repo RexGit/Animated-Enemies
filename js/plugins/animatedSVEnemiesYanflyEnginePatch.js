98ad9cb1068b8cb08d5f545d646caf039efd31ff
Rexal <rexalmail@gmail.com>
2015-10-30 16:43:23 -0700

    Patch 1.014
    Limited mouse support

--- js/plugins/animatedSVEnemiesYanflyEnginePatch.js
+++ js/plugins/animatedSVEnemiesYanflyEnginePatch.js
@@ -1,6 +1,6 @@
 //=============================================================================
 // AnimatedEnemiesYanflyCompatability.js
-// Version: 1.013rc
+// Version: 1.014
 //=============================================================================
 
 var Imported = Imported || {};
@@ -9,7 +9,7 @@ Imported.AnimatedSVEnemies.Yanfly = true;
 var Rexal = Rexal || {};
 Rexal.ASVE = Rexal.ASVE || {};
 /*:
- * @plugindesc Version: 1.013b
+ * @plugindesc Version: 1.014 - the clickening
  * - Makes animatedSVEnemies compatible with Yanfly's plugins.
  * @author Rexal
  *
@@ -21,54 +21,17 @@ Rexal.ASVE = Rexal.ASVE || {};
  v1.01 - got rid of an unnecessary function.
  v1.011 - Forgot a bracket.
  v1.012b - Should be compatible with the lastest Battle Engine Core.
- v1.013 - Fixed the positioning for when you set a different screen resolution
- v1.013b - Fixed the floating enemies...sorta.
+ v1.013 
+ - fixed enemy positioning...kinda.
+ - fixed floating monsters
+ v1.014 - fixed mouse support...but you can't mix up the enemy types.
  */
  if(Imported.AnimatedSVEnemies){
 	 
-	 
 
   //-----------------------------------------------------------------------------
-// Game_Enemy
+// Core Engine
 //=============================================================================
-
-	
-if(Yanfly.YEP_BattleEngineCore)
-{
-
-	Game_Enemy.prototype.spriteWidth = function() {
-    if (Rexal.ASVE._animated) {
-			return this.battler()._mainSprite.width;
-		} else {
-			return 1;
-		}
-};
-
-Game_Enemy.prototype.spriteHeight = function() {
-    if (Rexal.ASVE._animated) {
-			return this.battler()._mainSprite.height;
-		} else {
-			return 1;
-		}
-};
-
-}
-
-
-  //-----------------------------------------------------------------------------
-// Sprite_EnemyRex
-//=============================================================================
-
-if(Imported.YEP_BattleEngineCore)
-{
-	
-Sprite_EnemyRex.prototype.stepFlinch = function() {
-		var flinchX = this.x - this._homeX + Yanfly.Param.BECFlinchDist;
-		this.startMove(flinchX, 0, 6);
-};
-
-}
-
 
 if(Imported.YEP_CoreEngine && eval(Yanfly.Param.ReposBattlers))
 {
@@ -99,22 +62,61 @@ if(Imported.YEP_CoreEngine && eval(Yanfly.Param.ReposBattlers))
 			this.updatePosition();
 	};
 
-if(Imported.YEP_BattleEngineCore)
-{
-	
-	Sprite_EnemyRex.prototype.stepForward = function() {
-    this.startMove(Yanfly.Param.BECStepDist, 0, 12);
-};
-
-	
 }
 
-}
+
+
 
 
 
 
   //-----------------------------------------------------------------------------
-// Rex Functions
+// Battle Engine Core
 //=============================================================================
+
+if(Imported.YEP_BattleEngineCore)
+{
+	
+	
+	Game_Enemy.prototype.spriteWidth = function() {
+		
+    if(!this.battler()) return 1;
+	
+	if (Rexal.ASVE._animated) {
+			return this.battler()._mainSprite.width;
+		} else {
+			return this.battler().width;
+		}
+		
+};
+
+Game_Enemy.prototype.spriteHeight = function() {
+
+	    if(!this.battler()) return 1;
+		
+    if (Rexal.ASVE._animated) {
+			return this.battler()._mainSprite.height;
+		} else {
+			return this.battler().height;
+		}
+		
+};
+	
+Sprite_EnemyRex.prototype.stepFlinch = function() {
+		var flinchX = this.x - this._homeX + Yanfly.Param.BECFlinchDist;
+		this.startMove(flinchX, 0, 6);
+};
+
+	
+	Sprite_EnemyRex.prototype.stepForward = function() {
+    this.startMove(Yanfly.Param.BECStepDist, 0, 12);
+};
+
+
+
+
+}
+
+
+
  }
